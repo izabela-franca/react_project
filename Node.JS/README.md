46e693edd33ba6774ed7fb8a06bda01f1f33e09d
Izabela França <ifranca.sc@gmail.com>
2022-08-24 08:03:17 -0300

    Inserindo novos arquivos

---
+++ Node.JS/README.md
@@ -0,0 +1,5 @@
+A project made during a Bootcamp from TI Start to practice Node.Js. 
+
+________________________________________________________________________
+
+Projeto feito durante o Bootcamp TI Start para prática de Node.js.
---
+++ Node.JS/models/promocao.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Promocao extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Promocao.belongsTo(models.Empresa,
+        {foreignKey: 'EmpresaId', as: 'promocao_empresa'});
+      Promocao.belongsToMany(models.Cartao,
+        {foreignKey: 'CartaoId', through: 'Compra', as: 'promocao_cartao'});
+      Promocao.hasMany(models.Compra, 
+        {foreignKey: 'PromocaoId', as: 'promocao_compra'});
+    }
+  };
+  Promocao.init({
+    EmpresaId: DataTypes.INTEGER,
+    nome: DataTypes.STRING,
+    descricao: DataTypes.STRING,
+    validade: DataTypes.DATEONLY
+  }, {
+    sequelize,
+    modelName: 'Promocao',
+  });
+  return Promocao;
+};
