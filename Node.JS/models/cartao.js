3a1e453fca559a05a85ba80eb7600285504d84e4
Izabela França <ifranca.sc@gmail.com>
2022-08-24 07:30:11 -0300

    Inserindo novos arquivos

---
+++ Node.JS/models/cartao.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Cartao extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Cartao.belongsTo(models.Cliente,
+        {foreignKey: 'ClienteId', as: 'cartao_cliente'});
+      Cartao.belongsToMany(models.Promocao, 
+          {foreignKey: 'PromocaoId', through: 'Compra', as: 'cartao_promocao'});
+      Cartao.hasMany(models.Compra,
+        {foreignKey: 'CartaoId', as: 'cartao_compra'});
+    }
+  };
+  Cartao.init({
+    ClienteId: DataTypes.INTEGER,
+    dataCartao: DataTypes.DATEONLY,
+    validade: DataTypes.DATEONLY
+  }, {
+    sequelize,
+    modelName: 'Cartao',
+  });
+  return Cartao;
+};
